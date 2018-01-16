599a6cee573dc8f13e8bb107d9b742dc29f1d7fe
Grant-Inna <grantinna@mail.ru>
2018-01-16 18:48:16 +0300

    Started all new system

--- Video/gulpfile.js
+++ Video/gulpfile.js
@@ -170,7 +170,7 @@ gulp.task( 'watch__common', function() {
 
 
 gulp.task( 'watch__all', function() {
-    gulp.watch([ 'media.less', 'media-xs-c-outside.less', allSRC ], all);
+    gulp.watch([ 'media.less', '_var.less', 'media-xs-c-outside.less', allSRC ], all);
 });
 
 var all = [ 'CSS1', 'CSS2', 'CSS3', 'CSS4', 'CSS5', 'CSS6', 'CSS7', 'CSS8', 'CSS9', 'CSS10' ];
